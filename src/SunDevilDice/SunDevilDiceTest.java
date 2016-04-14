b320af024803ca39685b30fb38b1d3a699cb5cea
programmingprincess <jiaqiwu@asu.edu>
2016-04-14 15:45:26 -0700

    initialized JUnit tests

---
+++ src/SunDevilDice/SunDevilDiceTest.java
@@ -0,0 +1,14 @@
+package SunDevilDice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SunDevilDiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
